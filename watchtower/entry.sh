0b0ab777f0718d3d251acc1980ae2785b131a8a6
Denil Bhatt <denilbhatt2004@gmail.com>
2024-02-24 15:42:44 +0530

    MOD: watchtower

--- watchtower/entry.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Authenticate to ECR
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 970268366635.dkr.ecr.ap-south-1.amazonaws.com
-
-# Execute the original command (Watchtower in this case)
-exec "$@"
